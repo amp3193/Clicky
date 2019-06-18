dc0a1b044fe38a396b48397fa1c4821825c0158f
amp3193 <amp3193@yahoo.com>
2019-06-18 08:00:36 -0400

    Updates

---
+++ precache-manifest.f801afddcbc849c96156e92a4c2a96f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89572c9ecdac10a44aba7d310cb2e45",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "0ed0dd396e82b64c3043",
+    "url": "/Clicky/static/css/main.7ee258bf.chunk.css"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/Clicky/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "0ed0dd396e82b64c3043",
+    "url": "/Clicky/static/js/main.01f789d3.chunk.js"
+  },
+  {
+    "revision": "1726ed0b9997b87a72d7",
+    "url": "/Clicky/static/js/runtime~main.a36ce3a7.js"
+  }
+]);
